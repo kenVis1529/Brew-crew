4e0203e05dc187145441b9debb7a17d8342f7da0
Kien_Ta <tathuctrungkien2003@gmail.com>
2022-12-05 19:00:16 +0700

    Fixed error with Firebase.initializeApp()

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,14 @@ import 'package:brewcrew/screens/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+/// Error: PlatformException (PlatformException(channel-error, Unable to establish connection on channel., null, null))
+/// Vì thiết lập android với Firebase
+/// nên Firebase.initializeApp() chỉ chạy được trên Android device.
+/// Nếu không chạy được trên Android device, hãy thử cold boot.
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase
-      .initializeApp(); // Error: PlatformException (PlatformException(channel-error, Unable to establish connection on channel., null, null))
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
